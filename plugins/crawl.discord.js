42976cdf8340b7d73dc7f68e1c7d0fcee0633266
Jared Miller <jtmiller@gmail.com>
2017-08-03 17:36:42 -0400

    style some shit

--- plugins/crawl.discord.js
+++ plugins/crawl.discord.js
@@ -13,12 +13,34 @@ module.exports = {
 
   listeners: {
     'discord:say': function(evt, message) {
-      this.hook.send(this.stripColor(message));
+      // strip colors
+      message = this.stripColor(message);
+      
+      // check if rune or orb
+      if (/found (an? (\w+) rune)?(the Orb)? of Zot/.test(message)) {
+        message = this.addColor(message);
+      // check if death
+      } else if (/with \d+ points after \d+ turns/.test(message)) {
+        message = this.addBold(message);
+      // everything else gets ` `
+      } else {
+        message = "`" + message + "`";
+      }
+
+      this.hook.send(message);
     }
   },
 
   stripColor: function(item) {
     var re = /^{.*}/;
     return item.replace(re, "");
+  },
+
+  addColor: function(item) {
+    return "```http\n" + item + "\n```";
+  },
+
+  addBold: function(item) {
+    return "**" + item + "**";
   }
 };
